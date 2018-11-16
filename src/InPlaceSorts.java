7e2935ebd4564e08a19934245a697abec2bf46bd
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-11-16 10:12:10 -0500

    Finished selectionSort and used it in Runner. Need to work on other methods.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -4,12 +4,64 @@ public class InPlaceSorts
     {
 
     }
-    public static void selectionSort(double[] list1)
+
+    public static void selectionSort(double[] arr)
     {
-        
+        int swapPos = 0;
+        double min = 0;
+
+        for(int i = 0;i<arr.length;i++)
+        {
+            swapPos = i;
+            min = arr[i];
+
+            for(int a = 0;a<arr.length;a++)
+            {
+                if(arr[a]<min)
+                {
+                    swapPos = a;
+                    min = arr[a];
+
+                    swap(arr,swapPos,i);
+                }
+            }
+        }
     }
+
     public static void bubbleSort(String[] list1)
     {
 
+    }
+
+    //DELETE LATER BC NOT NEEDED
+    public static void bubbleSort(int[] arr)
+    {
+        int swapNum = 1;
+        while(swapNum != 0)
+        {
+            swapNum = 0;
+            for(int i=0;i<arr.length-1;i++)
+            {
+                if(arr[i]>arr[i+1])
+                {
+                    swap(arr,i,i+1);
+                    swapNum++;
+                }
+            }
+        }
+    }
+    //NOT NEEDED BC INT ARRAY
+    public static void swap(int[] arr,int pos1,int pos2)
+    {
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+
+    public static void swap(double[] arr,int pos1,int pos2)
+    {
+        double temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,41 @@
-public class Runner {
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        double[] check = randArr(1000);
+        printArr(check);
+
+        long start = System.currentTimeMillis();
+        //Place method here
+        InPlaceSorts.selectionSort(check);
+        long diff = System.currentTimeMillis() - start;
+        System.out.println(diff);
+    }
+
+    public static double[] randArr(int count)
+    {
+        double[] randArr = new double[count];
+
+        for(int i=0; i<randArr.length;i++)
+        {
+            randArr[i] = Math.random()*10001;
+        }
+        return(randArr);
+    }
+
+    public static void printArr(double[] arr)
+    {
+        String out = "";
+
+        for(int i =0;i<arr.length;i++)
+        {
+            out += arr[i];
+            if(i != arr.length-1)
+            {
+                out += ", ";
+            }
+        }
+        System.out.println(out);
+    }
 }
+
