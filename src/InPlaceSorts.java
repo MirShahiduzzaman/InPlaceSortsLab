b6e3bf06545f3b5e141d53dccbbfc9b8431ee52b
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-11-18 21:55:18 -0800

    Finishing up Project. Need to check if works.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -1,36 +1,90 @@
 public class InPlaceSorts
 {
+    /**
+     * Sorts a list of integers by using insertion sort
+     * @param list1 array to sort
+     */
     public static void insertionSort(int[] list1)
     {
+        int swapPos;
+        int min;
 
-    }
-
-    public static void selectionSort(double[] arr)
-    {
-        int swapPos = 0;
-        double min = 0;
-
-        for(int i = 0;i<arr.length;i++)
+        for(int i = 1;i<list1.length;i++)
         {
             swapPos = i;
-            min = arr[i];
+            min = list1[swapPos];
 
-            for(int a = 0;a<arr.length;a++)
+            for(int a = i-1;a>=0;a--)
             {
-                if(arr[a]<min)
+                if(list1[a]>min)
                 {
+                    swap(list1,swapPos,a);
                     swapPos = a;
-                    min = arr[a];
-
-                    swap(arr,swapPos,i);
+                    min = list1[swapPos];
+                }
+                else
+                {
+                    a=-1;
                 }
             }
         }
     }
 
+    /**
+     * Sorts a list of Doubles using selection sort
+     * @param list1 array to sort
+     */
+    public static void selectionSort(double[] list1)
+    {
+        int swapPos;
+        double min;
+
+        for(int i = 0;i<list1.length;i++)
+        {
+            swapPos = i;
+            min = list1[swapPos];
+
+            for(int a = i;a<list1.length;a++)
+            {
+                if(list1[a]<min)
+                {
+                    swapPos = a;
+                    min = list1[a];
+
+                    swap(list1,swapPos,i);
+                }
+            }
+        }
+    }
+
+
     public static void bubbleSort(String[] list1)
     {
-
+        int swapNum = 1;
+        while(swapNum!=0)
+        {
+            swapNum = 0;
+            for(int i = 0;i<list1.length-1;i++)
+            {
+                for(int a = 0;a<list1[i].length();a++)
+                {
+                    //checks if checking letter is at higher index than length of next word
+                    if(a>=list1[i+1].length())
+                    {
+                        swap(list1,i,i+1);
+                        swapNum++;
+                    }
+                    else
+                    {
+                        if(((list1[i].substring(a,a+1)).compareTo(list1[i+1].substring(a,a+1)))>0)
+                        {
+                            swap(list1,i,i+1);
+                            swapNum++;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     //DELETE LATER BC NOT NEEDED
@@ -50,7 +104,7 @@ public class InPlaceSorts
             }
         }
     }
-    //NOT NEEDED BC INT ARRAY
+
     public static void swap(int[] arr,int pos1,int pos2)
     {
         int temp = arr[pos1];
@@ -61,6 +115,13 @@ public class InPlaceSorts
     public static void swap(double[] arr,int pos1,int pos2)
     {
         double temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+
+    public static void swap(String[] arr,int pos1,int pos2)
+    {
+        String temp = arr[pos1];
         arr[pos1] = arr[pos2];
         arr[pos2] = temp;
     }
--- src/Runner.java
+++ src/Runner.java
@@ -2,18 +2,26 @@ public class Runner
 {
     public static void main(String[] args)
     {
-        double[] check = randArr(1000);
+        double[] check = randDoubleArr(1000);
         printArr(check);
 
         long start = System.currentTimeMillis();
-        //Place method here
         InPlaceSorts.selectionSort(check);
         long diff = System.currentTimeMillis() - start;
         printArr(check);
-        System.out.println(diff);
+        System.out.println(diff + "\n");
+
+        int[] check2 = randIntArr(1000);
+        printArr(check2);
+
+        start = System.currentTimeMillis();
+        InPlaceSorts.insertionSort(check2);
+        diff = System.currentTimeMillis() - start;
+        printArr(check2);
+        System.out.println(diff + "\n");
     }
 
-    public static double[] randArr(int count)
+    public static double[] randDoubleArr(int count)
     {
         double[] randArr = new double[count];
 
@@ -24,7 +32,73 @@ public class Runner
         return(randArr);
     }
 
+    public static int[] randIntArr(int count)
+    {
+        int[] randArr = new int[count];
+
+        for(int i=0; i<randArr.length;i++)
+        {
+            randArr[i] = (int)(Math.random()*10001);
+        }
+        return(randArr);
+    }
+
+    /**
+     * Makes a random array of Strings
+     * @param num the length of the array
+     * @param length the length of the Strings to be made
+     * @return array of random Strings
+     */
+    public String[] randStringArr(int num, int length)
+    {
+        String[] arr = new String[num];
+        while(num>0)
+        {
+            int i = 0;
+            String s = "";
+            while(i<length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s+c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return(arr);
+    }
+
     public static void printArr(double[] arr)
+    {
+        String out = "";
+
+        for(int i =0;i<arr.length;i++)
+        {
+            out += arr[i];
+            if(i != arr.length-1)
+            {
+                out += ", ";
+            }
+        }
+        System.out.println(out);
+    }
+
+    public static void printArr(int[] arr)
+    {
+        String out = "";
+
+        for(int i =0;i<arr.length;i++)
+        {
+            out += arr[i];
+            if(i != arr.length-1)
+            {
+                out += ", ";
+            }
+        }
+        System.out.println(out);
+    }
+
+    public static void printArr(String[] arr)
     {
         String out = "";
 
